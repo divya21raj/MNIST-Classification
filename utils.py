365057c344cde60dccb8a6d966c03ba0f876c8fe
Divya Raj <div21devel@gmail.com>
2020-03-28 17:12:13 +0530

    Added and Compared Random Forest Classifier

---
+++ utils.py
@@ -0,0 +1,7 @@
+def plot_precision_recall_vs_threshold(plt, precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g--", label="Recall")
+
+def plot_roc_curve(plt, fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')  # Dashed diagonal
